d13284e5f9a92f9874c9f436ba8efd4472aab765
pawelz00 <pawet.wets@gmail.com>
2025-03-06 13:33:06 +0100

    feat: query to get popular movies

---
+++ app/server/queries.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { Movie, MovieFetch } from "@/app/types";
+
+const OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.API_KEY}`,
+  },
+};
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+export async function getMovies(): Promise<Movie[] | undefined> {
+  try {
+    const [movies, genres] = await Promise.all([
+      fetch(`${BASE_URL}/movie/popular?language=en-US`, OPTIONS).then((res) =>
+        res.json(),
+      ),
+      fetch(`${BASE_URL}/genre/movie/list?language=en-US`, OPTIONS).then(
+        (res) => res.json(),
+      ),
+    ]);
+
+    const genresMap = genres.genres.reduce(
+      (
+        acc: { [x: string]: any },
+        genre: { id: string | number; name: any },
+      ) => {
+        acc[genre.id] = genre.name;
+        return acc;
+      },
+      {},
+    );
+
+    return movies.results.map((movie: MovieFetch) => ({
+      id: movie.id,
+      title: movie.title,
+      vote_average: movie.vote_average,
+      release_date: movie.release_date,
+      overview: movie.overview,
+      poster_url: movie.poster_path
+        ? `${IMAGE_BASE_URL}${movie.poster_path}`
+        : null,
+      genres: movie.genre_ids.map((id: number) => genresMap[id]),
+    }));
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
