6f219d65fb9da21200970081043b3f4b88c09244
pawelz00 <pawet.wets@gmail.com>
2025-03-06 13:33:17 +0100

    feat: basic types

---
+++ app/types.ts
@@ -0,0 +1,25 @@
+export interface MovieFetch {
+  adult: boolean;
+  id: number;
+  genre_ids: number[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Movie {
+  id: number;
+  title: string;
+  vote_average: number;
+  release_date: string;
+  overview: string;
+  poster_url: string | null;
+  genres: string[];
+}
